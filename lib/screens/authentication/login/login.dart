faaafa1d72a2f2abed42412a7879e2a8bef4ca0f
Kalidas P <kalidasp80@gmail.com>
2024-12-14 14:56:21 +0530

    login page added

--- lib/screens/authentication/login/login.dart
+++ lib/screens/authentication/login/login.dart
@@ -1,79 +1,152 @@
 import 'package:flutter/material.dart';
 
+import 'package:greenery/screens/widgets/custom_text.dart';
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final _formKey = GlobalKey<FormState>();
-    String email = '';
+    String username = '';
     String password = '';
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
+        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+              const SizedBox(height: 50.0),
+              // Top Image with Curved Borders
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Center(
+                  child: Image.asset(
+                    'assets/authentication/signup/Rectangle708.png', // Replace with your image path
+                    height: 229,
+                    width: 370,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  } else if (!RegExp(r'^\S+@\S+\.\S+\$').hasMatch(value)) {
-                    return 'Please enter a valid email';
-                  }
-                  return null;
-                },
-                onSaved: (value) => email = value!,
               ),
               const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
-                ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
-                  } else if (value.length < 6) {
-                    return 'Password must be at least 6 characters long';
-                  }
-                  return null;
-                },
-                onSaved: (value) => password = value!,
+              Text(
+                "Welcome Back!",
               ),
-              const SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    _formKey.currentState!.save();
-                    // Add your login logic here
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Logging in as $email')),
-                    );
-                  }
-                },
-                child: const Text('Login'),
+              Text(
+                "Let's login for explore bidding products",
               ),
               const SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Navigate to the signup page
-                  Navigator.pushNamed(context, '/signup');
-                },
-                child: const Text('Don\'t have an account? Sign up'),
+              // Login Form
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Username',
+                        border: const OutlineInputBorder(),
+                        prefixIcon: const Icon(Icons.person),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your username';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => username = value!,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        border: const OutlineInputBorder(),
+                        prefixIcon: const Icon(Icons.lock),
+                        suffixIcon: const Icon(Icons.visibility_off),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        } else if (value.length < 6) {
+                          return 'Password must be at least 6 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => password = value!,
+                    ),
+                    const SizedBox(height: 8.0),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {
+                          // Forgot Password Logic Here
+                        },
+                        child: CustomText(
+                          text: 'Forgot password?',
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          // Add Login Logic Here
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Logging in as $username'),
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text('Sign Up'),
+                    ),
+                    const SizedBox(height: 24.0),
+                    Center(
+                      child: CustomText(
+                        text: "You can connect with",
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    // Google Sign In Button
+                    OutlinedButton.icon(
+                      onPressed: () {
+                        // Google Sign-In Logic Here
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        side: const BorderSide(color: Colors.black12),
+                      ),
+                      label: CustomText(
+                        text: "Sign in with Google",
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    Center(
+                      child: TextButton(
+                        onPressed: () {
+                          // Navigate to Signup Page
+                          Navigator.pushNamed(context, '/signup');
+                        },
+                        child: CustomText(
+                          text: "Don't have an account? Sign Up",
+                          fontSize: 14,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ],
           ),
--- lib/screens/authentication/signup/sign_up.dart
+++ lib/screens/authentication/signup/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:greenery/screens/authentication/login/login.dart';
 import 'package:greenery/screens/widgets/custom_text.dart';
 
 class SignupPage extends StatelessWidget {
@@ -15,122 +16,142 @@ class SignupPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/authentication/signup/Rectangle708.png', // Replace with your image path
-              height: 229,
-              width: 370,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Center(
+                  child: Image.asset(
+                    'assets/authentication/signup/Rectangle708.png', // Replace with your image path
+                    height: 229,
+                    width: 370,
 
-              fit: BoxFit.cover,
-            ),
-            CustomText(text: "Let's Get Started", fontSize: 20),
-            CustomText(
-                text: "create an account to sell and buy your products ",
-                fontSize: 14),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'User Name',
-                      border: OutlineInputBorder(),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      } else if (!RegExp(r'^\S+@\S+\.\S+\$').hasMatch(value)) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => username = value!,
+                    fit: BoxFit.cover,
                   ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your email';
-                      } else if (!RegExp(r'^\S+@\S+\.\S+\$').hasMatch(value)) {
-                        return 'Please enter a valid email';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => email = value!,
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter your password';
-                      } else if (value.length < 6) {
-                        return 'Password must be at least 6 characters long';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => password = value!,
-                  ),
-                  const SizedBox(height: 16.0),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Confirm Password',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please confirm your password';
-                      } else if (value != password) {
-                        return 'Passwords do not match';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => confirmPassword = value!,
-                  ),
-                  const SizedBox(height: 24.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _formKey.currentState!.save();
-                        // Add your signup logic here
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Signing up as $email')),
-                        );
-                      }
-                    },
-                    child: const Text('Sign Up'),
-                  ),
-                  const SizedBox(height: 16.0),
-                  CustomText(text: "You can connect with", fontSize: 13),
-                  const SizedBox(height: 24.0),
-                  ElevatedButton(
-                      onPressed: () {},
-                      child: CustomText(
-                          text: " Sign Up with Google", fontSize: 13)),
-                  TextButton(
-                    onPressed: () {
-                      // Navigate back to the login page
-                      Navigator.pop(context);
-                    },
-                    child: const Text('Already have an account? Sign In'),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ],
+              CustomText(
+                text: "Let's Get Started",
+                fontStyle: FontStyle.values.first,
+                fontSize: 20,
+              ),
+              CustomText(
+                  text: "create an account to sell and buy your products ",
+                  fontSize: 14),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'User Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        } else if (!RegExp(r'^\S+@\S+\.\S+\$')
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => username = value!,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        } else if (!RegExp(r'^\S+@\S+\.\S+\$')
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => email = value!,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your password';
+                        } else if (value.length < 6) {
+                          return 'Password must be at least 6 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => password = value!,
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm Password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please confirm your password';
+                        } else if (value != password) {
+                          return 'Passwords do not match';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => confirmPassword = value!,
+                    ),
+                    const SizedBox(height: 24.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          // Add your signup logic here
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Signing up as $email')),
+                          );
+                        }
+                      },
+                      child: const Text('Sign Up'),
+                    ),
+                    const SizedBox(height: 16.0),
+                    Center(
+                        child: CustomText(
+                            text: "You can connect with", fontSize: 13)),
+                    const SizedBox(height: 15.0),
+                    ElevatedButton(
+                        onPressed: () {},
+                        child: CustomText(
+                            text: " Sign Up with Google", fontSize: 13)),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate back to the login page
+
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
+                      },
+                      child: const Text('Already have an account? Sign In'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
